3993d9870cf00c4a2fe62cb9ac8667e01d846c80
M4A1CID <konata_lzumi@hotmail.com>
2015-12-05 16:49:46 +0800

    Template for the Player class

---
+++ app/src/main/java/com/sidm/Senseless/PlayerClass.java
@@ -0,0 +1,15 @@
+package com.sidm.Senseless;
+
+/**
+ * Created by Edmund on 5/12/2015.
+ */
+public class PlayerClass {
+
+    int m_HealthPoints;
+    int m_Damage;
+    int m_Score;
+    int m_Gold;
+    int m_Type_1_Upgrade_Level;
+    int m_Type_2_Upgrade_Level;
+    int m_Time_Last_Attacked;
+}
